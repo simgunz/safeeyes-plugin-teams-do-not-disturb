15d34ad1cd2091a1d977160d2e8cc959abdb85a9
Simone Gaiarin <simone.gaiarin@teledyne.com>
2024-02-21 09:52:07 +0100

    Reimplement HTTP server without flask dependency

--- server/teams-presence.py
+++ server/teams-presence.py
@@ -1,20 +1,52 @@
-#import appdirs
-import flask
-from flask_cors import CORS
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 from pathlib import Path
 
-app = flask.Flask(__name__)
-CORS(app)
 
-@app.route("/", methods=['POST'])
-def hello_world():
-    if flask.request.method == 'POST':
-        status = flask.request.json['status']
-        write_status_to_file(status)
-        return "ok"
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
-def write_status_to_file(status):
-    #safeeyes_presence_file = Path(appdirs.user_data_dir()) / "safeeyes" / "teamspresence"
-    safeeyes_presence_file = Path().home() / ".local" / "share" / "safeeyes" / "teamspresence"
-    safeeyes_presence_file.parent.mkdir(exist_ok=True)
-    safeeyes_presence_file.write_text(status)
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.add_cors_headers()
+        self.end_headers()
+
+    def do_POST(self):
+        self.add_cors_headers()
+        length = int(self.headers["Content-Length"])
+        body = self.rfile.read(length).decode("utf-8")
+
+        try:
+            data = json.loads(body)
+            if "status" in data:
+                self.write_status_to_file(data["status"])
+                self.send_response(200)
+                self.send_header("Content-type", "text/plain; charset=utf-8")
+                self.end_headers()
+                self.wfile.write(b"ok")
+            else:
+                self.send_error(400, "Missing 'status' in JSON data")
+        except json.JSONDecodeError:
+            self.send_error(400, "Invalid JSON data")
+
+    def add_cors_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+
+    def write_status_to_file(self, status):
+        safeeyes_presence_file = (
+            Path.home() / ".local" / "share" / "safeeyes" / "teamspresence"
+        )
+        safeeyes_presence_file.parent.mkdir(exist_ok=True)
+        safeeyes_presence_file.write_text(status)
+
+
+def run(port=5000):
+    server_address = ("", port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print(f"Starting httpd on port {port}...")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
